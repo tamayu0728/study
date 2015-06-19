06375abf0496ba678955fb55d9cfdca50276005c
YukiTamada <tamada@ce.slis.tsukuba.ac.jp>
2015-06-19 17:13:15 +0900

    初めてのアプリ

--- Swift/sample1/sample1/ViewController.swift
+++ Swift/sample1/sample1/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var mainText: UITextField!
+    @IBAction func tap(sender: AnyObject) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        UIApplication.sharedApplication().setStatusBarStyle(UIStatusBarStyle.LightContent, animated:false) //ステータスバーの透明化
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
