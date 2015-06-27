f75c0a293887c42a16f769eda9b2f63398ae4cad
YukiTamada <tamada@ce.slis.tsukuba.ac.jp>
2015-06-27 22:05:17 +0900

    列挙型

--- C++/sample2/sample2/main.cpp
+++ C++/sample2/sample2/main.cpp
@@ -11,12 +11,14 @@
 //プロトタイプ宣言
 int DelNum(int,int);
 const int BUFFFA = 1000;
+enum Colors {red=100,brue=200,yellow,green,white};
 int main(int argc, const char * argv[]) {
 //    int ans = DelNum(10, 4);
 //    printf("%d\n",ans);
     for (int i=0; i<BUFFFA; i++) {
         printf("%d\n",i);
     }
+    printf("\n%d\n",red);
     return 0;
 }
 
