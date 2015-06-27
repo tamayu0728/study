1ed06642d96cbac144779b52813c08ed7c2c4558
YukiTamada <tamada@ce.slis.tsukuba.ac.jp>
2015-06-27 20:43:05 +0900

    C++の絵本_1章

---
+++ C++/sample1/sample1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sample1
+//
+//  Created by yuki tamada on 2015/06/27.
+//  Copyright (c) 2015年 CA Tech Kids. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
