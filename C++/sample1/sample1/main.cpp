e727ff77409478ca2ae5c310c371c5af421d5973
YukiTamada <tamada@ce.slis.tsukuba.ac.jp>
2015-06-27 21:19:06 +0900

    アクセス制御

--- C++/sample1/sample1/main.cpp
+++ C++/sample1/sample1/main.cpp
@@ -8,8 +8,40 @@
 
 #include <iostream>
 
+//クラスの定義
+class OrangeBox
+{
+private:
+    int total;
+    
+public:
+    void Add(int addOrange)
+    {
+        total += addOrange;
+    }
+    void Delete(int deleteOrange)
+    {
+        total -= deleteOrange;
+        if(total < 0){
+            Empty();
+        }
+    }
+    void Empty()
+    {
+        total = 0;
+    }
+    
+    int GetTotal()
+    {
+        return total;
+    }
+};
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    OrangeBox myOrangeBox;
+    myOrangeBox.Empty();
+    myOrangeBox.Add(10);
+    myOrangeBox.Delete(3);
+    printf("箱の中のみかん：%d個\n",myOrangeBox.GetTotal());
     return 0;
 }
