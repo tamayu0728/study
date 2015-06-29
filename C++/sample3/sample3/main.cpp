7f7b16ec7e342a916327a38a96ab7ded83f06250
YukiTamada <tamada@ce.slis.tsukuba.ac.jp>
2015-06-29 14:19:28 +0900

    オーバーロード、名前空間、インライン関数

--- C++/sample3/sample3/main.cpp
+++ C++/sample3/sample3/main.cpp
@@ -15,11 +15,36 @@ double GetCircleArea(int radius,double pi)
     return radius * radius * pi;
 }
 
+using namespace std;
+
+inline int MyCalc(int a,int b)
+{
+    return a+b;
+}
+
+//オーバーロード
+/*
 int main(int argc, const char * argv[]) {
     double area1,area2,area3;
     area1 = GetCircleArea();
     area2 = GetCircleArea(10);
     area3 = GetCircleArea(15,3);
     printf("%lf,%lf,%lf\n",area1,area2,area3);
+    return 0;
+}
+*/
+
+//名前空間
+/*
+int main(int argc, const char * argv[]) {
+    cout << "コンチワ" << endl;
+    return 0;
+}
+*/
+
+int main()
+{
+    int i = MyCalc(10,30);
+    cout << i << endl;
     return 0;
 }
